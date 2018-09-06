7aaf82df3243cbae60cff1a7092e18941d96b7e4
Sylas <saldridge@flowroute.com>
2018-09-06 16:02:30 -0700

    html template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 from pick import pickacon
+from flask import render_template
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def pick_con():
     response = pickacon()
-    return response
+    return render_template('index.html', title="Create-A-Conspiracy", response = response)
